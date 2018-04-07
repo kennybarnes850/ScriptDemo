751b781b8ae57ab928a5629f7483b10487e71b64
Kenneth Barnes <kennethbarnes@Kenneths-MacBook-Air.local>
2018-04-06 21:53:22 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//This prompts the user to input their name//
+var userName = prompt("What is your name?");
+
+//This will alert the user that it is nice to meet them//
+alert("Nice to meet you, " + userName + "!");
+
+//This will post in the console//
+console.log("Nice to meet you too, " + userName + "!");
